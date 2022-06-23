8e1584766a456be9dfb8034f5a930d91a7640412
PS <ps@dc.ag>
2022-06-23 17:41:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Auto Assign Assignees From Team
+
+Assign any amount of members from a given [GitHub Team](https://help.github.com/en/github/setting-up-and-managing-organizations-and-teams/organizing-members-into-teams) as Assignees to a PR
+Members are chosen randomly
+
+## Example Usage
+
+```yaml
+name: "Assign Assignees"
+on:
+  pull_request:
+    types: [opened, ready_for_review]
+
+jobs:
+  assign-reviewers:
+    runs-on: ubuntu-latest
+    steps:
+      - name: "Assign assignees from team"
+        uses: rowi1de/auto-assign-assignees-from-team@v1.0.0
+        with:
+          repo-token: ${{ secrets.GITHUB_TOKEN }}
+          org: "org" # organization containing the team
+          team: "team" # the team name
+          amount: 0 # amount of assignees to assign from the given team, 0 to assign all. If the amount exceeds the member count of the team all members will be added
+```
+
+## Build
+```shell
+npm run build &&  npm run package
+```
---
+++ src/main.ts
@@ -0,0 +1,93 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+// eslint-disable-next-line require-jsdoc
+export async function run() {
+  try {
+    const repoToken = core.getInput("repo-token", { required: true });
+    const team = core.getInput("teams");
+    const org = core.getInput("org");
+    const amount = parseInt(core.getInput("amount"));
+
+    const issue: { owner: string; repo: string; number: number } =
+      github.context.issue;
+
+    if (issue == null || issue.number == null) {
+      console.log("No pull request context, skipping");
+      return;
+    }
+
+    // See https://octokit.github.io/rest.js/
+    const client = github.getOctokit(repoToken);
+
+    const members = await client.rest.teams.listMembersInOrg({
+      org: org,
+      team_slug: team,
+    });
+    console.log("Request Status for getting team members:" + members.status);
+    // filter out PR author
+    let memberNames = members.data.map((a) => a.login);
+    console.log("Picking reviewer from members:", memberNames);
+
+    let finalAssignees: string[] = [];
+
+    if (amount === 0 || memberNames.length <= amount) {
+      finalAssignees = memberNames;
+    } else {
+      memberNames = shuffle(memberNames);
+      for (let i = 0; i < amount; i++) {
+        const name = memberNames.pop();
+        if (name !== undefined) {
+          finalAssignees.push(name);
+        }
+      }
+    }
+
+    if (finalAssignees.length > 0) {
+      const personResponse = await client.rest.pulls.requestReviewers({
+        owner: issue.owner,
+        repo: issue.repo,
+        pull_number: issue.number,
+        reviewers: finalAssignees,
+      });
+      console.log(
+        "Request Status:" +
+          personResponse.status +
+          ", Assignees from Team " +
+          team +
+          ":" +
+          personResponse?.data?.requested_reviewers
+            ?.map((r) => r.login)
+            .join(",")
+      );
+    } else {
+      console.log("No members to assign found in team " + team);
+    }
+  } catch (error) {
+    console.error(error);
+    core.setFailed("Unknown error" + error);
+    throw error;
+  }
+}
+
+function shuffle<T>(array: T[]): T[] {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+run();
